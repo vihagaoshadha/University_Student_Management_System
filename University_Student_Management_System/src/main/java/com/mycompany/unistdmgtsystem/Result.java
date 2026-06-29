8854bf439edadb64c4212b36e43a7692e96641ca
HansikaTharushi <ediriweerahansika2003@gmail.com>
2026-06-29 22:51:06 +0530

    Add private variables and both constructors for Result class

--- University_Student_Management_System/src/main/java/com/mycompany/unistdmgtsystem/Result.java
+++ University_Student_Management_System/src/main/java/com/mycompany/unistdmgtsystem/Result.java
@@ -9,5 +9,35 @@ package com.mycompany.unistdmgtsystem;
  * @author M9
  */
 public class Result {
+    //---- private variables----
+
+    private String studentId;
+    private String courseCode;
+    private int academicYear;
+    private  int semester;
+    private double marks;
+    private String grade;
+    private double gradePoint;
+
+    // ---- Constructo 01 -----
+    public Result(String studentId, Course course, double marks){
+        this.studentId = studentId;
+        this.courseCode = course.getCourseCode();    //get course code from course
+        this.academicYear = course.getAcademicYear(); //get academic year from course
+        this.semester = course.getSemester();         //get semester from course
+        setMarks(marks);
+    }
+
+    //---- Constructor with all parameters -----
+          public Result(String studentId, String courseCode,int academicYear, int semester,double marks, String grade, double gradePoint) {
+ 
+        this.studentId    = studentId;
+        this.courseCode   = courseCode;
+        this.academicYear = academicYear;
+        this.semester     = semester;
+        this.marks        = marks;
+        this.grade        = grade;
+        this.gradePoint   = gradePoint;
+    }
     
 }
