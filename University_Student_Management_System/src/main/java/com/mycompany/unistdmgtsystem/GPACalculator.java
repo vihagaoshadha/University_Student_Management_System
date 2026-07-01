dd3005711a38346b8e4b5687d377bc75030fdacd
vidurachathuranga77-stack <vidurachathuranga77@gmail.com>
2026-07-01 16:13:58 +0530

    calculate Grade and calculate Grade Point

--- University_Student_Management_System/src/main/java/com/mycompany/unistdmgtsystem/GPACalculator.java
+++ University_Student_Management_System/src/main/java/com/mycompany/unistdmgtsystem/GPACalculator.java
@@ -8,6 +8,49 @@ package com.mycompany.unistdmgtsystem;
  *
  * @author M9
  */
+
 public class GPACalculator {
-    
+      // Calculate Grade
+    public static String calculateGrade(double marks) {
+
+        if (marks >= 85)
+            return "A";
+        else if (marks >= 75)
+            return "B+";
+        else if (marks >= 70)
+            return "B";
+        else if (marks >= 65)
+            return "C+";
+        else if (marks >= 60)
+            return "C";
+        else if (marks >= 55)
+            return "D+";
+        else if (marks >= 50)
+            return "D";
+        else
+            return "E";
+    }
+     // Calculate Grade Point
+    public static double calculateGradePoint(double marks) {
+
+        if (marks >= 85)
+            return 4.0;
+        else if (marks >= 75)
+            return 3.7;
+        else if (marks >= 70)
+            return 3.3;
+        else if (marks >= 65)
+            return 3.0;
+        else if (marks >= 60)
+            return 2.7;
+        else if (marks >= 55)
+            return 2.3;
+        else if (marks >= 50)
+            return 2.0;
+        else
+            return 0.0;
+    }
+
 }
+
+
